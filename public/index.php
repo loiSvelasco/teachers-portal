a7ddb75e1a77d5bd54460937baf863f04b7e02a4
Louis Velasco <21021463+loiSvelasco@users.noreply.github.com>
2022-10-20 16:38:21 +0800

    added bootstrap

--- public/index.php
+++ public/index.php
@@ -4,33 +4,36 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Teacher's Portal</title>
     <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <link href="../bootstrap/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="../bootstrap/bootstrap-icons.css">
 </head>
 <body class="d-flex flex-column h-100">
     <nav class="py-2 bg-dark border-bottom">
         <div class="container d-flex flex-wrap">
         <ul class="nav me-auto">
-            <li class="nav-item"><a href="#" class="nav-link link-light px-2" disabled>Home</a></li>
-            <li class="nav-item"><a href="#" class="nav-link link-light px-2">Features</a></li>
+            <li class="nav-item"><a href="#" class="nav-link link-light px-2">Trainings</a></li>
+            <li class="nav-item"><a href="#" class="nav-link link-light px-2">Programs</a></li>
         </ul>
         <ul class="nav">
+            <li class="nav-item px-2"><a class="nav-link link-light" target="_blank" href="https://depedilocosnorte.com"><strong>Schools Division of Ilocos Norte</strong></a></li>
+        </ul>
+        <!-- <ul class="nav">
             <li class="nav-item"><a href="#" class="nav-link link-light px-2">Login</a></li>
             <li class="nav-item"><a href="#" class="nav-link link-light px-2">Sign up</a></li>
-        </ul>
+        </ul> -->
         </div>
     </nav>
     <header class="py-3 mb-4 border-bottom">
         <div class="container d-flex flex-wrap justify-content-center">
         <a href="/" class="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none">
             <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-            <span class="fs-4">Double header</span>
+            <span class="text-muted">Teacher's Portal</span>
         </a>
-        <form class="col-12 col-lg-auto mb-3 mb-lg-0" role="search">
+        <!-- <form class="col-12 col-lg-auto mb-3 mb-lg-0" role="search">
             <input type="search" class="form-control rounded-0" placeholder="Search..." aria-label="Search">
-        </form>
+        </form> -->
         </div>
     </header>
 
@@ -117,6 +120,6 @@
     </div>
     </footer>
     <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <script src="../bootstrap/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
 </html>
